dd1452211849114300a6ef0adba82e25cf2d63a0
Conor Heine <conor.heine@gmail.com>
2023-01-22 14:51:00 -0800

    add alembic

--- hitfactorpy_sqlalchemy/orm/models.py
+++ hitfactorpy_sqlalchemy/orm/models.py
@@ -1,61 +1,186 @@
-from typing import Any, Dict
-
+import inflection
 import sqlalchemy as sa
 from hitfactorpy.enums import Classification, Division, MatchLevel, PowerFactor, Scoring
 from sqlalchemy.dialects.postgresql import UUID
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base, declarative_mixin, declared_attr, relationship, validates  # type: ignore
+from sqlalchemy.orm.attributes import Mapped  # type: ignore
 from sqlalchemy_continuum import make_versioned
 
 make_versioned(user_cls=None)
 
+"""
+https://docs.sqlalchemy.org/en/20/core/type_basics.html#sqlalchemy.types.Enum.params.name
+"""
+ENUM_PREFIX = "HITFACTORPY_ENUM_"
+
+
+def _validate_positive_int(value: int, name: str):
+    if not value >= 0:
+        raise ValueError(f'value for  "{name}" must be >=0, but received: {value}')
+    return value
+
+
+class _BaseCls:
+    """https://docs.sqlalchemy.org/en/13/orm/extensions/declarative/mixins.html#augmenting-the-base"""
+
+    @declared_attr
+    def __tablename__(cls):
+        return inflection.underscore(cls.__name__)
+
+
 """Base ORM Model"""
-BaseModel = declarative_base()
+SABaseModel = declarative_base(cls=_BaseCls)
 
 
-class ModelWithId(BaseModel):  # type: ignore
-    """Model with a database-internal id and a public uuid"""
+@declarative_mixin
+class MixinVersioned:
+    """Mixin requirements for sqlalchemy-continuum"""
 
+    @declared_attr
+    def __versioned__(cls):
+        return {}
+
+
+@declarative_mixin
+class MixinIds:
+    """Mixin columns for a database-internal id as the primary key, and a public uuid"""
+
+    id = sa.Column(
+        sa.Integer,
+        primary_key=True,
+        autoincrement=True,
+        comment="Internal ID to be used as PK and FKs. Do not expose in public APIs",
+    )
+    uuid = sa.Column(
+        UUID(as_uuid=True),
+        nullable=False,
+        unique=True,
+        index=True,
+        server_default=sa.text("gen_random_uuid()"),
+        comment="ID exposed in public APIs",
+    )
+
+
+class BaseModel(SABaseModel, MixinIds):  # type: ignore
+    __abstract__ = True
+
+
+class VersionedModel(BaseModel):  # , MixinVersioned):
+    __abstract__ = True
+
+
+SAEnumMatchLevel = sa.Enum(MatchLevel, create_constraint=True, name=f"{ENUM_PREFIX}MatchReportMatchLevel")
+
+
+class MatchReport(VersionedModel):
+
+    # Columns
+    name = sa.Column(sa.Unicode(255), nullable=False)
+    date = sa.Column(sa.Date)
+    match_level = sa.Column(SAEnumMatchLevel)
+
+    # Relationships
+    competitors: Mapped["MatchReportCompetitor"] = relationship(
+        "MatchReportCompetitor",
+        back_populates="match",
+        cascade="all, delete",
+        passive_deletes=True,
+        primaryjoin="MatchReport.id==MatchReportCompetitor.match_id",
+    )
+    stages: Mapped["MatchReportStage"] = relationship(
+        "MatchReportStage",
+        back_populates="match",
+        cascade="all, delete",
+        passive_deletes=True,
+        primaryjoin="MatchReport.id==MatchReportStage.match_id",
+    )
+    stage_scores: Mapped["MatchReportStageScore"] = relationship(
+        "MatchReportStageScore",
+        back_populates="match",
+        cascade="all, delete",
+        passive_deletes=True,
+        primaryjoin="MatchReport.id==MatchReportStageScore.match_id",
+    )
+
+
+SAEnumDivision = sa.Enum(Division, create_constraint=True, name=f"{ENUM_PREFIX}MatchReportCompetitorDivision")
+SAEnumClassification = sa.Enum(
+    Classification, create_constraint=True, name=f"{ENUM_PREFIX}MatchReportCompetitorClassification"
+)
+SAEnumPowerFactor = sa.Enum(PowerFactor, create_constraint=True, name=f"{ENUM_PREFIX}MatchReportCompetitorPowerFactor")
+
+
+class MatchReportCompetitor(VersionedModel):
     id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
     uuid = sa.Column(
         UUID(as_uuid=True), nullable=False, unique=True, index=True, server_default=sa.text("gen_random_uuid()")
     )
-
-
-class VersionedModel(ModelWithId):
-    """Model with auto-managed versioning via sqlalchemy-continuum"""
-
-    __versioned__: Dict[Any, Any] = {}
-
-
-class MatchReportCompetitor(VersionedModel):
-    __tablename__ = "match_report_competitor"
-
+    # Columns
+    match_id = sa.Column(sa.Integer, sa.ForeignKey(MatchReport.id, ondelete="CASCADE"), nullable=False)
     member_number = sa.Column(sa.Unicode(64))
     first_name = sa.Column(sa.Unicode(64))
     last_name = sa.Column(sa.Unicode(64))
-    division = sa.Column(Division)
-    classification = sa.Column(Classification)
-    power_factor = sa.Column(PowerFactor)
+    division = sa.Column(SAEnumDivision)
+    classification = sa.Column(SAEnumClassification)
+    power_factor = sa.Column(SAEnumPowerFactor)
     dq = sa.Column(sa.Boolean, nullable=False, default=False)
     reentry = sa.Column(sa.Boolean, nullable=False, default=False)
 
+    # Relationships
+    match: Mapped[MatchReport] = relationship(MatchReport, uselist=False, back_populates="competitors")
+    stage_scores: Mapped["MatchReportStageScore"] = relationship("MatchReportStageScore", back_populates="competitor")
+
+
+SAEnumScoring = sa.Enum(Scoring, create_constraint=True, name=f"{ENUM_PREFIX}MatchReportStageScoring")
+
 
 class MatchReportStage(VersionedModel):
-    __tablename__ = "match_report_stage"
-
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    uuid = sa.Column(
+        UUID(as_uuid=True), nullable=False, unique=True, index=True, server_default=sa.text("gen_random_uuid()")
+    )
+    # Columns
+    match_id = sa.Column(sa.Integer, sa.ForeignKey(MatchReport.id, ondelete="CASCADE"), nullable=False)
     name = sa.Column(sa.Unicode(255))
     min_rounds = sa.Column(sa.Integer, nullable=False)
     max_points = sa.Column(sa.Integer, nullable=False)
     classifier = sa.Column(sa.Boolean, nullable=False, default=False)
     classifier_number = sa.Column(sa.Unicode(64))
-    scoring_type = sa.Column(Scoring, nullable=False, default=Scoring.COMSTOCK)
+    scoring_type = sa.Column(
+        SAEnumScoring,
+        nullable=False,
+        default=Scoring.COMSTOCK,
+    )
+
+    # relationships
+    match: Mapped[MatchReport] = relationship(MatchReport, uselist=False, back_populates="stages")
+    stage_scores: Mapped["MatchReportStageScore"] = relationship(
+        "MatchReportStageScore",
+        back_populates="stage",
+        cascade="all, delete",
+        passive_deletes=True,
+        primaryjoin="MatchReportStage.id==MatchReportStageScore.stage_id",
+    )
+
+    # Validators
+    @validates("min_rounds")
+    def validate_min_rounds(self, key, value):
+        return _validate_positive_int(value, key)
+
+    @validates("max_points")
+    def validate_max_points(self, key, value):
+        return _validate_positive_int(value, key)
 
 
 class MatchReportStageScore(VersionedModel):
-    __tablename__ = "match_report_stage_score"
-
-    competitor_id = sa.Column(sa.Integer, nullable=False)
-    stage_id = sa.Column(sa.Integer, nullable=False)
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    uuid = sa.Column(
+        UUID(as_uuid=True), nullable=False, unique=True, index=True, server_default=sa.text("gen_random_uuid()")
+    )
+    # Columns
+    match_id = sa.Column(sa.Integer, sa.ForeignKey(MatchReport.id, ondelete="CASCADE"), nullable=False)
+    competitor_id = sa.Column(sa.Integer, sa.ForeignKey(MatchReportCompetitor.id, ondelete="CASCADE"), nullable=False)
+    stage_id = sa.Column(sa.Integer, sa.ForeignKey(MatchReportStage.id, ondelete="CASCADE"), nullable=False)
     a = sa.Column(sa.Integer, nullable=False, default=0)
     b = sa.Column(sa.Integer, nullable=False, default=0)
     c = sa.Column(sa.Integer, nullable=False, default=0)
@@ -77,10 +202,41 @@ class MatchReportStageScore(VersionedModel):
     dq = sa.Column(sa.Boolean, nullable=False, default=False)
     dnf = sa.Column(sa.Boolean, nullable=False, default=False)
 
+    # Relationships
+    match: Mapped[MatchReport] = relationship(MatchReport, uselist=False, back_populates="stage_scores")
+    competitor: Mapped[MatchReportCompetitor] = relationship(
+        MatchReportCompetitor, uselist=False, back_populates="stage_scores"
+    )
+    stage: Mapped[MatchReportStage] = relationship(MatchReportStage, uselist=False, back_populates="stage_scores")
 
-class MatchReport(VersionedModel):
-    __tablename__ = "match_report"
+    # Validators
+    @validates("a")
+    def validate_a(self, key, value):
+        return _validate_positive_int(value, key)
 
-    name = sa.Column(sa.Unicode(255), nullable=False)
-    date = sa.Column(sa.Date)
-    match_level = sa.Column(MatchLevel)
+    @validates("b")
+    def validate_b(self, key, value):
+        return _validate_positive_int(value, key)
+
+    @validates("c")
+    def validate_c(self, key, value):
+        return _validate_positive_int(value, key)
+
+    @validates("d")
+    def validate_d(self, key, value):
+        return _validate_positive_int(value, key)
+
+    @validates("m")
+    def validate_m(self, key, value):
+        return _validate_positive_int(value, key)
+
+    @validates("ns")
+    def validate_ns(self, key, value):
+        return _validate_positive_int(value, key)
+
+    @validates("npm")
+    def validate_npm(self, key, value):
+        return _validate_positive_int(value, key)
+
+
+sa.orm.configure_mappers()  # Must be called immediately after the last model definition
